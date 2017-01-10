1e141b7822501b5872a0ecee7f43ad28e1b01874
Thoon <thon.anders@gmail.com>
2017-01-10 14:40:21 +0100

    Fix

--- src/main/java/springmvc/domene/MonitorResults.java
+++ src/main/java/springmvc/domene/MonitorResults.java
@@ -20,8 +20,9 @@ public class MonitorResults {
     private String customerName;
     private String customerAddress;
     public double percentage;
+    private String streng;
 
-    public MonitorResults(int serialnumber, double maxWeight, double currentWeight, Timestamp timestamp, int supplierId, int customerId, String customerName, String customerAddress, double percentage) {
+    public MonitorResults(int serialnumber, double maxWeight, double currentWeight, Timestamp timestamp, int supplierId, int customerId, String customerName, String customerAddress, double percentage, String streng) {
         this.serialnumber = serialnumber;
         this.maxWeight = maxWeight;
         this.currentWeight = currentWeight;
@@ -30,6 +31,7 @@ public class MonitorResults {
         this.customerName = customerName;
         this.customerAddress = customerAddress;
         this.percentage = percentage;
+        this.streng = streng;
     }
     
     public MonitorResults(){};
@@ -98,9 +100,17 @@ public class MonitorResults {
         this.percentage = p;
     }
     
+    public String getStreng(){
+        return streng;
+    }
+    
+    public void setStreng(String streng){
+        this.streng = streng;
+    }
+    
 
     @Override
     public String toString() {
-        return "MonitorResults{" + "serialnumber=" + serialnumber + ", maxWeight=" + maxWeight + ", currentWeight=" + currentWeight + ", timestamp=" + timestamp + ", customerId=" + customerId + ", customerName=" + customerName + ", customerAddress=" + customerAddress + ", percentage=" + percentage + '}';
+        return "serialnumber= " + serialnumber + ", customerName= " + customerName + ", customerAddress= " + customerAddress + ", maxWeight= " + maxWeight + ", percentage= " + percentage + ", timestamp= " + timestamp;
     }
 }
